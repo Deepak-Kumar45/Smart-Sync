af070e48d4e1dcd4fa9779c15a91a5302f2d2f46
Deepak-Kumar45 <kumardeep4591@gmail.com>
2024-09-07 13:53:17 +0530

    First commit

---
+++ src/main/java/com/smartsync/SmartSyncApplication.java
@@ -0,0 +1,14 @@
+package com.smartsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSyncApplication.class, args);
+		System.out.println("\n\t!!..Application has been started..!!");
+	}
+
+}
---
+++ src/test/java/com/smartsync/SmartSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
