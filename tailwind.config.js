81c3ebacfc68e58fbc0428392c155e91d4eaf4d2
Deepak-Kumar45 <kumardeep4591@gmail.com>
2024-09-09 23:42:42 +0530

    sign-up form changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/main/resources/**/*.{html,js}"],
+  content: ["./src/main/resources/**/*.{html,css,js}"],
   theme: {
     extend: {},
   },
