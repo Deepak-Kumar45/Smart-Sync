d6ef7dc580b8dd5e0aaf09dacb2bc9dae09bc689
Deepak-Kumar45 <kumardeep4591@gmail.com>
2024-09-15 18:10:12 +0530

    user section added

---
+++ src/main/java/com/smartsync/controllers/SmartUserController.java
@@ -0,0 +1,22 @@
+package com.smartsync.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/user")
+public class SmartUserController {
+
+    @GetMapping("/dashboard")
+    public String userDashboard() {
+        return "user/dashboard";
+    }
+    
+    @GetMapping("/profile")
+    public String userProfile() {
+        return "user/profile";
+    }
+    
+}
